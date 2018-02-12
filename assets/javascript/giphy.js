9ed2fba3c90cfac2ba122b1a28d3fc12b5526474
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-02-12 11:02:57 -0600

    reduced random offset

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     //displays gifs in #gif-area
     function displayGifs() {
 
-        var offset = Math.floor(Math.random() * 101);
+        var offset = Math.floor(Math.random() * 11);
 
         //prevents reloading the page
         event.preventDefault();
